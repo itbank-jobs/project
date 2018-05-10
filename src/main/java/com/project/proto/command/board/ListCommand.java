2f1a0f811697d444f3ee083343de9c1e9e8abcc4
DESKTOP-UHBUSIP\user <mix25rt@naver.com>
2018-05-10 15:21:53 +0900

    게시판 글 올리기 계속 수정해야 함.

--- src/main/java/com/project/proto/command/board/ListCommand.java
+++ src/main/java/com/project/proto/command/board/ListCommand.java
@@ -1,8 +1,12 @@
 package com.project.proto.command.board;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.ui.Model;
 
 import com.project.proto.dao.board_Dao;
+import com.project.proto.dto.board_Dto;
 
 
 
@@ -11,6 +15,13 @@ public class ListCommand implements Command {
 
 	@Override
 	public void execute(Model model, board_Dao dao) {
+		
+		
+		
+		List<board_Dto> list = dao.list();
+		
+		model.addAttribute("list", list);
+		
 	
 	}
 
